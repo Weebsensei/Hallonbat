9781fdb29c81981600443c53006d555a8fc030b1
Weebsensei <e.pettersson.63@hotmail.com>
2025-11-04 06:56:22 +0100

    updates

--- F1.py
+++ F1.py
@@ -25,7 +25,7 @@ class Hallonbåt :
 # colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 Drivers = []
 Hallonbåtar = []
-RacesSheet = 'CD'#EFGHIJKLMNOPQRSTUVWXYZ'
+RacesSheet = 'CDEFGHIJKLMNOPQRSTUVWXYZ'
 Races = ["Australia","China","Japan","Bahrain","Saudi","Miami","Imola","Monaco","Spain","Canada","Austria","Britain","Belgium","Hungary","Netherlands","Italy","Azerbaijan","Singapore","Austin","Mexico","Brazil","Las Vegas","Qatar","Abu Dhabi"]
 
 ## Build a List for all Drivers and one for all HallonbåtGP teams ##
